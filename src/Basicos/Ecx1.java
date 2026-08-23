032cfdf86bc2026ed6246cc974959a96e7a38cfb
David Vanegas <Davidvg09@hotmail.com>
2026-08-23 18:39:52 -0500

    Exercise 1 Hecho

--- src/Basicos/Ecx1.java
+++ src/Basicos/Ecx1.java
@@ -1,5 +1,12 @@
 package Basicos;
-
+import java.util.Scanner;
 public class Ecx1 {
-
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int edad = 46;
+        String fullName = "David Vanegas";
+        double height = 1.67;
+        Boolean status = true;
+        System.out.println("Hola soy : " + fullName + ", Mi edad es: " + edad + "años, Mi estatura es de: " + height + "y mi estado de matricula es: " + status);
+    }
 }
