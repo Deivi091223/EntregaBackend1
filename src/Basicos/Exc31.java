44094e327686160835d8c80fd801259b16fea8c4
David Vanegas <Davidvg09@hotmail.com>
2026-08-27 16:57:07 -0500

    Exercice 31 completo

---
+++ src/Basicos/Exc31.java
@@ -0,0 +1,20 @@
+package Basicos;
+
+import java.util.Scanner;
+
+public class Exc31 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int[]nums= new int[5];
+        int i;
+        for(i=0;i<5;i++){
+            System.out.println("Digite um numero en posicion " + i + ": ");
+            nums[i]=leer.nextInt();
+        }
+        i=0;
+        while(i<5){
+            System.out.println("Numero en pocision " + i + ": " + nums[i]);
+            i++;
+        }
+    }
+}
